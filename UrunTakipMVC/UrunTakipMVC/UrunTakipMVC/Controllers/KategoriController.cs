21378df9afe1cdc84086448b379a6b08d3dc5a89
Gani Ozturk <ganioztrk@gmail.com>
2023-05-09 21:35:48 +0300

    update

---
+++ UrunTakipMVC/UrunTakipMVC/UrunTakipMVC/Controllers/KategoriController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UrunTakipMVC.Models.Entity;
+
+namespace UrunTakipMVC.Controllers
+{
+    public class KategoriController : Controller
+    {
+        private DbStokMVCEntities db = new DbStokMVCEntities();
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            var values = db.Kategorilers.Where(x => x.Durum == true).ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategoriler kategoriler)
+        {
+            db.Kategorilers.Add(kategoriler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriSil(Kategoriler kategoriler)
+        {
+            var kategori = db.Kategorilers.Find(kategoriler.ID);
+            kategori.Durum = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult KategoriGuncelle(int id)
+        {
+            var kategori = db.Kategorilers.Find(id);
+            return View("KategoriGuncelle", kategori);
+        }
+        [HttpPost]
+        public ActionResult KategoriGuncelle(Kategoriler kategoriler)
+        {
+            var kategori = db.Kategorilers.Find(kategoriler.ID);
+            kategori.Ad = kategoriler.Ad;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ UrunTakipMVC/UrunTakipMVC/UrunTakipMVC/Controllers/UrunController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UrunTakipMVC.Models.Entity;
+
+namespace UrunTakipMVC.Controllers
+{
+    public class UrunController : Controller
+    {
+        DbStokMVCEntities db = new DbStokMVCEntities();
+        public ActionResult Index()
+        {
+            var urunler = db.Urunlers.Where(x=>x.Durum == true).ToList();
+            return View(urunler);
+        }
+
+        [HttpGet]
+        public ActionResult UrunEkle()
+        {
+            List<SelectListItem> linqkategori = (from x in db.Kategorilers.ToList()
+                select new SelectListItem
+                {
+                    Text = x.Ad,
+                    Value = x.ID.ToString()
+                }).ToList();
+            ViewBag.vKategori = linqkategori;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UrunEkle(Urunler urunler)
+        {
+            var kategori = db.Kategorilers.FirstOrDefault(x => x.ID == urunler.Kategoriler.ID);
+            urunler.Kategoriler = kategori;
+            db.Urunlers.Add(urunler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UrunGuncelle(int id)
+        {
+            List<SelectListItem> linqktgr = (from x in db.Kategorilers.ToList()
+                select new SelectListItem
+                {
+                    Text = x.Ad,
+                    Value = x.ID.ToString()
+                }).ToList();
+            var urun = db.Urunlers.Find(id);
+            ViewBag.kategoriler = linqktgr;
+            return View("UrunGuncelle", urun);
+        }
+
+        [HttpPost]
+        public ActionResult UrunGuncelle(Urunler urunler)
+        {
+            var urun = db.Urunlers.Find(urunler.ID);
+            urun.Ad = urunler.Ad;
+            urun.Marka = urunler.Marka;
+            urun.Stok = urunler.Stok;
+            urun.AlisFiyat = urunler.AlisFiyat;
+            urun.SatisFiyat = urunler.SatisFiyat;
+            var ktg = db.Kategorilers.FirstOrDefault(x => x.ID == urunler.Kategoriler.ID);
+            urun.Kategori = ktg.ID;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UrunSil(Urunler urunler)
+        {
+            var urun = db.Urunlers.Find(urunler.ID);
+            urun.Durum = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
